f0703f74c433af926cd623dafdfe218b57c85755
huyansheng <huyansheng@didichuxing.com>
2019-08-06 20:17:00 +0800

    增加hooks简单源码实现

--- src/container/custom-hooks-more.js
+++ src/container/custom-hooks-more.js
@@ -1,30 +1,45 @@
 import React from 'react'
 
-import { useState } from 'react'
+// import { useState } from 'react'
 
-// import { useHooks, useState } from "../utils/useHooks";
+import { useHooks, useState } from "../utils/useHooks";
 
-
-function CustomHooks(props) {
+function CustomHooks1(props) {
     const [name, setName] = useState('胡衍生')
-
-    const [title, setTitle] = useState('信控')
-
-    console.log('CustomHooks')
-
+    console.log('CustomHooks1')
     return (
         <div>
             <div>
                 <label htmlFor="">姓名</label>
                 <input type="text" value={name} onChange={e => setName(e.target.value)} />
             </div>
+        </div>
+    );
+}
 
+function CustomHooks2(props) {
+    const [title, setTitle] = useState('信控')
+    console.log('CustomHooks2')
+    return (
+        <div>
             <div>
                 <label htmlFor="">标题</label>
                 <input type="text" value={title} onChange={e => setTitle(e.target.value)} />
             </div>
         </div>
     );
+}
+
+CustomHooks1 = useHooks(CustomHooks1);
+CustomHooks2 = useHooks(CustomHooks2);
+
+function CustomHooks() {
+    return (
+        <div>
+            <CustomHooks1></CustomHooks1>
+            <CustomHooks2></CustomHooks2>
+        </div>
+    )
 }
 
 export default CustomHooks
--- src/utils/useHooks.js
+++ src/utils/useHooks.js
@@ -1,5 +1,56 @@
+import React, { Component } from 'react'
+
+let instance
+
+function setCurrentInstance(ins) {
+    instance = ins
+    instance.__index__ = 0
+}
+
+function resetCurrentInstanceIndex(ins) {
+    instance.__index__ = 0
+}
 
 
-export function useHooks() { }
+export function useHooks(SFC) {
+    return class extends Component {
+        render() {
+            setCurrentInstance(this)
+            return <SFC {...this.props} />
+        }
 
-export function useState() { }
+        componentDidUpdate(prevProps, prevState) {
+            resetCurrentInstanceIndex(this)
+        }
+
+        componentDidMount() {
+            resetCurrentInstanceIndex(this)
+        }
+    }
+}
+
+export function useState(initialValue) {
+    const [value, setValue] = getData(instance, initialValue)
+    return [value, setValue]
+}
+
+function getData(instance, initialValue) {
+    if (!instance.__states___) instance.__states___ = []
+
+    // 首次render
+    if (!instance.__isNotFirstRender___) {
+        const index = instance.__index__
+        function setValue(val) {
+            instance.__states___[index][0] = val
+            instance.forceUpdate()
+        }
+        instance.__states___.push([
+            initialValue,
+            setValue
+        ])
+    }
+
+    const result = instance.__states___[instance.__index__]
+    instance.__index__ += 1
+    return result
+}
